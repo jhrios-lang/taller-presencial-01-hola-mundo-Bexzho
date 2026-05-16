8322a31db2c9b814c76210e51c73c6fde2dc5f03
jhrios-lang <jhrios@unal.edu.co>
2026-05-16 09:23:50 -0500

    Intento 1
    Intento 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -14,7 +14,7 @@ def pregunta_01():
 
     """
 
-    return "Hola mundo cruel"
+    return "Hola mundo cruel!"
 
 
 if __name__ == "__main__":
--- homework/pregunta_02.py
+++ homework/pregunta_02.py
@@ -14,7 +14,7 @@ def pregunta_02():
 
     """
 
-    return "Hello cruel world"
+    return "Hello cruel world!"
 
 
 if __name__ == "__main__":
